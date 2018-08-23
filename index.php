96b6ccc51044c804d39871d7e84f3b1129d09bfa
Lola OLIVET <lolivet@student.42.fr>
2018-08-23 18:20:22 +0200

    Gallery created

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/gallery.css">
+    <title>Camagruuu</title>
+  </head>
+  <body>
+    <header>
+      <h1>Camagru</h1>
+    </header>
+    <div id="top"></div>
+    <div class="gallery">
+    	<div class="row">
+    		<ul>
+    			<a href="#" class="close"></a>
+    			<li>
+    				<a href="#item02">
+    					<img src="https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg?auto=compress&cs=tinysrgb&h=350" alt="">
+    				</a>
+    			</li>
+
+    			<li>
+    				<a href="#item02">
+    					<img src="https://shop.usa.canon.com/wcsstore/CanonB2BStoreFrontAssetStore/images/Landscapes-Made-Easy_4_pdp.jpg" alt="">
+    				</a>
+    			</li>
+
+    			<li>
+    				<a href="#item02">
+    					<img src="https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?auto=compress&cs=tinysrgb&h=350" alt="">
+    				</a>
+    			</li>
+    			<li>
+    				<a class="image" href="#item01">
+    					<img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg" alt="">
+    				</a>
+    			</li>
+    			<li>
+    				<a class="image" href="#item02">
+    					<img src="https://iso.500px.com/wp-content/uploads/2018/02/500px_blog_landscape_photography_quest-1500x1000.jpg" alt="">
+    				</a>
+    			</li>
+    			<li>
+    				<a class="image" href="#item01">
+    					<img src="https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg" alt="">
+    				</a>
+    			</li>
+    			<li>
+    				<a class="image" href="#item01">
+    					<img src="https://lonelyplanetwpnews.imgix.net/2018/02/1st-Max-Rive-8396.jpg" alt="">
+    				</a>
+    			</li>
+    		</ul>
+    	</div> <!-- / row -->
+</div>
+    	<!-- Item 01 -->
+    	<div id="item01" class="port">
+
+    		<div class="row">
+    			<div class="description">
+    				<h1>Item 01</h1>
+    				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis libero erat. Integer ac purus est. Proin erat mi, pulvinar ut magna eget, consectetur auctor turpis.</p>
+    			</div>
+
+    				<img src="https://cdn.dribbble.com/users/545884/screenshots/3981307/lorena2.png" alt="">
+    			</div>
+    		</div> <!-- / row -->
+
+    	</div> <!-- / Item 02 -->
+
+    	<!-- Item 02 -->
+    	<div id="item02" class="port">
+
+    		<div class="row">
+    			<div class="description">
+    				<h1>Item 02</h1>
+    				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis libero erat. Integer ac purus est. Proin erat mi, pulvinar ut magna eget, consectetur auctor turpis.</p>
+    			</div>
+    			<img src="https://cdn.dribbble.com/users/545884/screenshots/2883479/cover.jpg" alt="">
+    		</div> <!-- / row -->
+
+    	</div> <!-- / Item 02 -->
+
+
+
+
+
+  </body>
+</html>
